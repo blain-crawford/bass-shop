81a2d597f67a8b9ed40eb1b1a7c9454035ec2107
Blain Crawford <blaincrawford@gmail.com>
2022-06-07 14:18:05 -0400

    all APIs done

---
+++ src/components/products/allProducts.js
@@ -0,0 +1,7 @@
+import basses from './basses';
+import bassAmps from './bassAmps';
+import bassPedals from './bassPedals';
+
+const allProducts = [...basses, ...bassAmps, ...bassPedals];
+
+export default allProducts;
---
+++ src/components/products/bassPedals.js
@@ -0,0 +1,212 @@
+const bassPedals = [
+  {
+    id: '56042680',
+    title:
+      'Electro-Harmonix Black Russian Big Muff Pi Sovtek',
+    condition: 'Mint',
+    pricing: {
+      display: '$750',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--X-GBoFBJ--/t_card-square/v1654448125/xo1rvpfgy1gsm4ezh5fq.jpg',
+      'https://images.reverb.com/image/upload/s--J65W41Cc--/t_card-square/v1654448126/oonay4ageb6rnvwqzjyk.jpg',
+      'https://images.reverb.com/image/upload/s--4bWThSdk--/t_card-square/v1654448125/b4sc2zhj81cz4fbzgi0o.jpg',
+    ],
+  },
+  {
+    id: '56076100',
+    title: 'Aguilar Filter Twin Dual Envelope Bass Filter Pedal',
+    condition: 'Excellent',
+    pricing: {
+      display: '$190',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--kzx9Xcyw--/t_card-square/v1654540746/sjb98h81b2ginnfuxicy.jpg',
+      'https://images.reverb.com/image/upload/s--eipgt-mJ--/t_card-square/v1654540762/jq7vdgvhoh0wgy787kpk.jpg',
+      'https://images.reverb.com/image/upload/s--9x_Tg0Jy--/t_card-square/v1654540791/bfrns48dqohu585c0gxs.jpg',
+    ],
+  },
+  {
+    id: '56089958',
+    title: 'Electro-Harmonix Bass Mono Synth Effects Pedal',
+    condition: 'Brand New',
+    pricing: {
+      display: '$200',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--gTH1k8tL--/t_card-square/v1654573293/b2fksuaivgnxpecrftca.png',
+    ],
+  },
+  {
+    id: '55625602',
+    title: 'Dunlop CBM105Q Cry Baby Mini Bass Wah',
+    condition: 'Excellent',
+    pricing: {
+      display: '$109.99',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--od9c8-5N--/t_card-square/v1653596709/vjz2j2tvqw3qk175r3lm.jpg',
+      'https://images.reverb.com/image/upload/s--yTB6Lcz0--/t_card-square/v1653596710/wbp7wxyxvvv6dqkudyin.jpg',
+    ],
+  },
+  {
+    id: '55987931',
+    title:
+      'MXR BASS ENVELOPE FILTER M82',
+    condition: 'Excellent',
+    pricing: {
+      display: '$110',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--k0Ra6z4D--/t_card-square/v1654286429/a2kk6hdzvwfckekllix4.jpg',
+      'https://images.reverb.com/image/upload/s--Jyyq0zTi--/t_card-square/v1654286432/dqnsvoulcinrl3ifu1lk.jpg',
+      'https://images.reverb.com/image/upload/s--uQQb_qc3--/t_card-square/v1654286435/aeywcbk3mhshabn0lbio.jpg',
+    ],
+  },
+  {
+    id: '55906791',
+    title: 'Digitech Bass Synth Wah FX Synthesizer Pedal',
+    condition: 'Excellent',
+    pricing: {
+      display: '$185',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--5DgmhHid--/a_270/t_card-square/v1654129773/txcq97vdhpwophzenbro.jpg',
+      'https://images.reverb.com/image/upload/s--WZb2kA-g--/t_card-square/v1654129777/lvhakj078gkymly0vfn5.jpg',
+      'https://images.reverb.com/image/upload/s--4aMX2Y00--/t_card-square/v1654129775/hwdv3nlyebegiebdf3q4.jpg',
+    ],
+  },
+  {
+    id: '53003828',
+    title: 'Line 6 Bass POD xt Multi-Effect and Amp Modeler',
+    condition: 'Excellent',
+    pricing: {
+      display: '$92',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--xD72kpIL--/t_card-square/v1649019217/rqvwzjsio3xh8srackve.jpg',
+      'https://images.reverb.com/image/upload/s--orkmZJDL--/t_card-square/v1649019220/ddihtzzo8xaskwqoqb5h.jpg',
+      'https://images.reverb.com/image/upload/s--jC4wNKCD--/t_card-square/v1649019223/ya9fzi9c788zjhvgeqzi.jpg',
+    ],
+  },
+  {
+    id: '55989473',
+    title: 'Music man Joe Dart II #33',
+    condition: 'Mint',
+    pricing: {
+      display: '$3500',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--7Vf63vGb--/t_card-square/v1654289497/uis3hhapciu8jgfw5qm7.jpg',
+      'https://images.reverb.com/image/upload/s--bJCEjrRc--/t_card-square/v1654289492/qct07f275e8dc9ovy3r4.jpg',
+      'https://images.reverb.com/image/upload/s--L79HAaWW--/t_card-square/v1654289492/dkfnsy2jlf7vq90sytf8.jpg',
+    ],
+  },
+  {
+    id: '39408792',
+    title: 'Line 6 Helix LT Guitar Bass Multi effects Processor',
+    condition: 'Brand New',
+    pricing: {
+      display: '$1199.99',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--XmQcstLi--/t_card-square/v1615645824/qtu3ldeusmbtnchqwkun.jpg',
+      'https://images.reverb.com/image/upload/s--h41zl56l--/t_card-square/v1615645820/eh1y2kjbofwxfvkcf9h4.jpg',
+    ],
+  },
+  {
+    id: '56081462',
+    title: 'Electro-Harmonix Micro POG',
+    condition: 'Very Good',
+    pricing: {
+      display: '$160',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--TeFQFpTM--/t_card-square/v1654552114/kooeascr3oedzhrdyllr.webp',
+      'https://images.reverb.com/image/upload/s--iGcBvw9G--/t_card-square/v1654552185/ogmv3gtqmhithlpb42mp.webp',
+      'https://images.reverb.com/image/upload/s--4OS5bufN--/t_card-square/v1654552186/qd2hbfm3vouyhxefgsmp.webp',
+    ],
+  },
+  {
+    id: '56022578',
+    title: 'Electro-Harmonix POG 2 Octave Generator',
+    condition: 'Very Good',
+    pricing: {
+      display: '$270',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--T8mxt69J--/t_card-square/v1654380498/asphy1biwnpxzroads9p.jpg',
+      'https://images.reverb.com/image/upload/s--sByA-5vp--/t_card-square/v1654380499/zxedww2vwgnnuhc6iu4u.jpg',
+      'https://images.reverb.com/image/upload/s--1Cmkb9LQ--/t_card-square/v1654380500/yvflrhh4otbhlecacjwf.jpg',
+    ],
+  },
+  {
+    id: '56005386',
+    title: 'TC Electronic SpectraComp Bass Compressor Pedal',
+    condition: 'Very Good',
+    pricing: {
+      display: '$85',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--DC8FmuQw--/t_card-square/v1654345705/dvpvfcxke3ihp1vtpq93.jpg',
+      'https://images.reverb.com/image/upload/s--iFtg9ZqG--/t_card-square/v1654345755/dut6qkhxp0cdayeidozg.jpg',
+      'https://images.reverb.com/image/upload/s--_L7saGuc--/t_card-square/v1654345722/biykydnmtlpboufb5zaw.jpg',
+    ],
+  },
+  {
+    id: '56089465',
+    title: 'Alembic Spoiler Cocobolo 7 String Bass',
+    condition: 'Excellent',
+    pricing: {
+      display: '$5799.99',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--t_XfPJAU--/t_card-square/v1654571386/h3meeubsw2chb2olysld.jpg',
+      'https://images.reverb.com/image/upload/s--KvW5qymN--/t_card-square/v1654571394/ctmnxjdeyzvin3lbs4xo.jpg',
+      'https://images.reverb.com/image/upload/s--43WdHUqp--/t_card-square/v1654571402/x1es8iwk87aoiw6k0cer.jpg',
+    ],
+  },
+  {
+    id: '55491761',
+    title: 'Pigtronix Philosopher Bass Compressor PBC',
+    condition: 'Good',
+    pricing: {
+      display: '$90',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--GdMnr9ki--/t_card-square/v1653417572/r6hrhxyjdlg3odls6chz.jpg',
+      'https://images.reverb.com/image/upload/s--IFfiAlld--/t_card-square/v1653417620/ueqfkcfah3gpnmyjevk7.jpg',
+      'https://images.reverb.com/image/upload/s---yJucbUM--/t_card-square/v1653417586/pfzp6sjbiflit5q9mjc5.jpg',
+    ],
+  },
+  {
+    id: '55540675',
+    title: 'Keeley Bassist Compressor',
+    condition: 'Excellent',
+    pricing: {
+      display: '$199',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--16yI_Fn9--/t_card-square/v1653489182/m3rqowixdkmg326i6qqm.jpg',
+      'https://images.reverb.com/image/upload/s---NmIOVJO--/t_card-square/v1653489182/iia5qoct5s1uwj1uk5dc.jpg',
+      'https://images.reverb.com/image/upload/s--MzM_COlV--/t_card-square/v1653489182/cm9c8g2k4yeiw1sgsxkv.jpg',
+    ],
+  },
+];
+
+export default bassPedals;

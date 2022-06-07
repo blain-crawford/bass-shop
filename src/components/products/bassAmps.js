9a7356137c78bea1e43b232f9398fe751ad32e1a
Blain Crawford <blaincrawford@gmail.com>
2022-06-07 11:28:42 -0400

    Bass amp api done

---
+++ src/components/products/bassAmps.js
@@ -0,0 +1,210 @@
+const bassAmps = [
+  {
+    id: '55132747',
+    title:
+      'GrBass One 800',
+    condition: 'Very Good',
+    pricing: {
+      display: '$559.92',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--ftvCaai_--/t_card-square/v1652786039/iajummhy1b6hwkk8a6p1.jpg',
+      'https://images.reverb.com/image/upload/s--JcPOz76W--/t_card-square/v1652786038/mio0e92im5yyqms077l9.jpg',
+      'https://images.reverb.com/image/upload/s---wi5BAf6--/t_card-square/v1654109701/w7f4aqnitq4x4odnomd2.jpg',
+    ],
+  },
+  {
+    id: '55195194',
+    title: 'Traynor YBA-1A Bass Master Mark II 90-Watt Guitar Amp Head',
+    condition: 'Good',
+    pricing: {
+      display: '$700',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--UW2-hsGE--/t_card-square/v1652907340/jyrpdygvhyut3glviyam.jpg',
+      'https://images.reverb.com/image/upload/s--BaCGRglk--/t_card-square/v1652907333/i08zhsiffrgzjaa1tq3m.jpg',
+      'https://images.reverb.com/image/upload/s--k8pqgnAE--/t_card-square/v1652907337/ichjbavhybd3mo823zz9.jpg',
+    ],
+  },
+  {
+    id: '50129238',
+    title: 'Guild Thunderbass Head',
+    condition: 'Very Good',
+    pricing: {
+      display: '$550',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--Q7if9_n6--/t_card-square/v1643577120/uhzwj6xjnrw63noy5n0l.jpg',
+      'https://images.reverb.com/image/upload/s---DKGS-Vd--/t_card-square/v1643577110/wyojja3a6fbvtzla75ze.jpg',
+      'https://images.reverb.com/image/upload/s---x2xkzq2--/t_card-square/v1643577113/slrl7xy6vpzzpokuu19r.jpg',
+    ],
+  },
+  {
+    id: '54559187',
+    title: 'Ampeg SVT Head & 8-10 Cabinet Combo 1981 Japan MTI Original',
+    condition: 'Very Good',
+    pricing: {
+      display: '$2000',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--zjUMuKNf--/t_card-square/v1651696460/ltqshlvcvxxjyn6tgtek.jpg',
+      'https://images.reverb.com/image/upload/s--NWgrGhpV--/t_card-square/v1651696647/u4udj5sw4ut6mnxqixlp.jpg',
+      'https://images.reverb.com/image/upload/s--3zJnvi_C--/t_card-square/v1651696750/bt4gznn88txnoydwyj7v.jpg',
+    ],
+  },
+  {
+    id: '50211428',
+    title:
+      'Mesa Boogie Big Block 750 Bass Amplifier (San Diego, CA)',
+    condition: 'Very Good',
+    pricing: {
+      display: '$799.99',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--lpGrbCDy--/t_card-square/v1643753197/dwfwhouc5sf7swam3rkv.jpg',
+      'https://images.reverb.com/image/upload/s--uCOuTZ11--/t_card-square/v1643753196/rqdvgsmgab0mvlz9qdlp.jpg',
+      'https://images.reverb.com/image/upload/s--nXaqb73p--/t_card-square/v1643753196/kateakqsssfnq1re0dvm.jpg',
+    ],
+  },
+  {
+    id: '39103287',
+    title: 'Blackstar U120 Unity 120 1x12 120-Watt Bass Combo',
+    condition: 'Brand New',
+    pricing: {
+      display: '$391.67',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--tlx9Wk1v--/t_card-square/v1614431971/qwelkpg8bsodvpyrlroo.png',
+      'https://images.reverb.com/image/upload/s--c7u5MJTT--/t_card-square/v1614431980/feyd2kwp6cnjkyo5b05d.png',
+      'https://images.reverb.com/image/upload/s--H2wjo8nr--/t_card-square/v1614431985/a3l1qucal0dkx6jk5wmu.png',
+    ],
+  },
+  {
+    id: '55756506',
+    title: 'AMPEG BA 600 - 115 BLACK',
+    condition: 'Excellent',
+    pricing: {
+      display: '$1119.83',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--QHgCpWdv--/t_card-square/v1653853270/mp9teq8ox1wvxgjuqawf.jpg',
+      'https://images.reverb.com/image/upload/s--t5vOy32g--/t_card-square/v1653853277/u4azpbt4zkgiysvsv3hs.jpg',
+      'https://images.reverb.com/image/upload/s--rhV3ty-R--/t_card-square/v1653853283/xbv2a9alndufgvnhemxj.jpg',
+    ],
+  },
+  {
+    id: '55466781',
+    title: 'Fender Musicmaster Bass 2-Channel 12-Watt 1x12 Bass Combo 1970 - 1980',
+    condition: 'Very Good',
+    pricing: {
+      display: '$675',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--n8tXdX9M--/t_card-square/v1653378894/qtbmhlip3wkytj86b482.jpg',
+      'https://images.reverb.com/image/upload/s--D6DoqvCX--/t_card-square/v1653378896/vaym5z2zmju3by2uydr6.jpg',
+    ],
+  },
+  {
+    id: '52184567',
+    title: 'SWR 2x10c',
+    condition: 'Very Good',
+    pricing: {
+      display: '$350',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--1tH8tXzu--/t_card-square/v1647551721/dfqk1mvetmjootxqru8u.jpg',
+      'https://images.reverb.com/image/upload/s--OklkoeV---/t_card-square/v1647551742/ptep2ihztj7q0speyzix.jpg',
+      'https://images.reverb.com/image/upload/s--XI2lIxN4--/t_card-square/v1647551863/qwem3vu2jpzizcime0c6.jpg',
+    ],
+  },
+  {
+    id: '55383631',
+    title: 'Acoustic 360/361 Bass Preamp and Powered Cabinet 1968-1971',
+    condition: 'Very Good',
+    pricing: {
+      display: '$1800',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--LJBhqjHO--/t_card-square/v1653177208/lnlflo8zvqxzur5oij6d.jpg',
+      'https://images.reverb.com/image/upload/s--acMxXhhY--/t_card-square/v1653177209/uiuqbjoucsssk9onujvb.jpg',
+      'https://images.reverb.com/image/upload/s--GtOSCVyv--/t_card-square/v1653177210/gggovapeeili8pyv3ror.jpg',
+    ],
+  },
+  {
+    id: '56026194',
+    title: 'Phil Jones Bass  Briefcase & PB100 Expansion cabinet',
+    condition: 'Mint',
+    pricing: {
+      display: '$700',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--bZZuNVbE--/t_card-square/v1654393512/nqprpucfftaqgphdkvyl.jpg',
+      'https://images.reverb.com/image/upload/s--sAZiMbsa--/t_card-square/v1654393524/ltnwy0hu8ykotauk8orj.jpg',
+      'https://images.reverb.com/image/upload/s--sMHwiCzL--/t_card-square/v1654393534/cxvonau0ejpe4qg4rsdk.jpg',
+    ],
+  },
+  {
+    id: '55630995',
+    title: 'Mesa Boogie PowerHouse Standard PH 1000 4x10 / 1x15',
+    condition: 'Very Good',
+    pricing: {
+      display: '$550',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--0J_2cKYf--/c_crop,h_0.500,w_1.000,x_0.000,y_0.281/t_card-square/v1653608280/uizd07gwmqb74tktlx8c.png',
+    ],
+  },
+  {
+    id: '54383673',
+    title: 'Markbass MBL100043 Blackline Standard 104HR 200-Watt 4x10',
+    condition: 'Excellent',
+    pricing: {
+      display: '$650',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--dQjvLljw--/t_card-square/v1651340887/dfupbdr1ahatktwb27ut.jpg',
+    ],
+  },
+  {
+    id: '55135649',
+    title: 'Hartke HyDrive HD112 300-Watt 1x12 Bass Speaker Cabinet',
+    condition: 'Very Good',
+    pricing: {
+      display: '$350',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--Adw0w2e9--/t_card-square/v1652795069/hoo3yvfxayqulifhxhhi.jpg',
+      'https://images.reverb.com/image/upload/s--QjdLOQj6--/t_card-square/v1652795068/fmdrrgnzwez5vufugw46.jpg',
+      'https://images.reverb.com/image/upload/s--lpppjifF--/t_card-square/v1652795069/ngkqwbtjf2fjsvsiq4v9.jpg',
+    ],
+  },
+  {
+    id: '53956097',
+    title: 'IAtlas Bass Speaker Cabinet 6x12',
+    condition: 'Excellent',
+    pricing: {
+      display: '$800',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--uktEXMHF--/t_card-square/v1650566908/nqaq78es4pcav9iktctz.jpg',
+      'https://images.reverb.com/image/upload/s--B9Kovr71--/t_card-square/v1650566907/ryta75znfdtqqczf2qaz.jpg',
+    ],
+  },
+];
+
+export default bassAmps;

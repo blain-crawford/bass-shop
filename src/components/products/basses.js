ac4bd83f182b822d3617869538b15d584e5f9a01
Blain Crawford <blaincrawford@gmail.com>
2022-06-07 10:53:36 -0400

    bass AAPI built, going on to do amps and pedals

---
+++ src/components/products/basses.js
@@ -0,0 +1,231 @@
+const basses = [
+  {
+    id: '55898663',
+    title:
+      'Fender Deluxe Active Jazz Bass White w/Rosewood Fretboard and Gig Bag',
+    condition: 'excellent',
+    strings: 4,
+    pricing: {
+      display: '$699',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--CFeXFkYD--/t_card-square/v1654109719/y6hjbaebw73bwpvqbfc3.jpg',
+      'https://images.reverb.com/image/upload/s--y3_RWM_d--/t_card-square/v1654109723/dtyoakjnhvinc4u1besc.jpg',
+      'https://images.reverb.com/image/upload/s---wi5BAf6--/t_card-square/v1654109701/w7f4aqnitq4x4odnomd2.jpg',
+    ],
+  },
+  {
+    id: '14359651',
+    title: 'Barlow Guitars Falcon II 4 String Bass Proto 2018 Camatillo',
+    condition: 'Brand New',
+    strings: 4,
+    pricing: {
+      display: '$10,000',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--nSadY53t--/t_card-square/v1650524805/nrpaojwwhxgbd8wnkfqs.webp',
+      'https://images.reverb.com/image/upload/s--gCe918mx--/t_card-square/v1534322096/xwoibpi7raen9g5q4b8v.jpg',
+      'https://images.reverb.com/image/upload/s--l2um_HzA--/t_card-square/v1537755122/n72ql7kxtvaoatmsfray.jpg',
+    ],
+  },
+  {
+    id: '31557155',
+    title: 'Fender Japan Black Label Jazz Bass MIJ Rare Japanese ULTRA RARE',
+    condition: 'Very Good',
+    strings: 4,
+    pricing: {
+      display: '$1799',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--KLVgvDpA--/t_card-square/v1579849959/xe4x4abrq7efngpkoj2b.jpg',
+      'https://images.reverb.com/image/upload/s--ILJ31g_y--/t_card-square/v1579849968/e2iwese99a42uoqiv5vu.jpg',
+      'https://images.reverb.com/image/upload/s--cm-nW96O--/t_card-square/v1579849954/nqxldw8ozr6wrs48wqui.jpg',
+    ],
+  },
+  {
+    id: '56108648',
+    title: 'Warwick Pro Series Team Built Thumb Bolt-On 6-String',
+    condition: 'Mint',
+    strings: 6,
+    pricing: {
+      display: '$2600',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--rjtmjWft--/t_card-square/v1654609602/tpiam8sasezyinmjov9y.jpg',
+      'https://images.reverb.com/image/upload/s--eiU0l38o--/t_card-square/v1654609658/xgpkw20i2dqm0racxwm4.jpg',
+      'https://images.reverb.com/image/upload/s--xPkfD7Oe--/t_card-square/v1654609709/iy8prtojoxobmwf9brag.jpg',
+    ],
+  },
+  {
+    id: '52954619',
+    title:
+      'Bianca Halstead of Betty Blowtorch & Butt Trumpet - Fender Precision Bass',
+    condition: 'Mint',
+    strings: 4,
+    pricing: {
+      display: '$50,000',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--kyQ_GG76--/c_crop,h_0.667,w_1.000,x_0.000,y_0.333/t_card-square/v1648950154/ex9hozdhz2c8g2cmzzpu.jpg',
+      'https://images.reverb.com/image/upload/s--cQQsyYMY--/t_card-square/v1648950002/qu5dgpasc3b2iyuxyufs.jpg',
+      'https://images.reverb.com/image/upload/s--mhSV6ZmQ--/t_card-square/v1648950007/s2iaaoh9ccteszbdn5gb.jpg',
+    ],
+  },
+  {
+    id: '55958204',
+    title: 'Fender Player Precision Bass',
+    condition: 'Excellent',
+    strings: 4,
+    pricing: {
+      display: '$740',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--9PSnP4N2--/t_card-square/v1654230151/sxpgpw2ahbrkmdnms7k6.jpg',
+      'https://images.reverb.com/image/upload/s--tfWPmVek--/t_card-square/v1654230150/nlwqopyptxnkh9qeozwf.jpg',
+      'https://images.reverb.com/image/upload/s--3GzByYEs--/t_card-square/v1654230151/jymgvq3qbhx4vr3ffqvr.jpg',
+    ],
+  },
+  {
+    id: '56104550',
+    title: 'Fodera Victor Wooten Yin Yang Deluxe 5 Monarch',
+    condition: 'Very Good',
+    strings: 5,
+    pricing: {
+      display: '$8953.05',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--XtFdiqFu--/t_card-square/v1654602898/s2woqaxqyf7qe7vvnelh.jpg',
+      'https://images.reverb.com/image/upload/s--e4smXEsE--/t_card-square/v1654602896/kapnqztgoyjwsf14punr.jpg',
+      'https://images.reverb.com/image/upload/s--Ec3iQ-_W--/t_card-square/v1654602901/kl3hyzzgua8ntdky0tqk.jpg',
+    ],
+  },
+  {
+    id: '55989473',
+    title: 'Music man Joe Dart II #33',
+    condition: 'Mint',
+    strings: 4,
+    pricing: {
+      display: '$3500',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--7Vf63vGb--/t_card-square/v1654289497/uis3hhapciu8jgfw5qm7.jpg',
+      'https://images.reverb.com/image/upload/s--bJCEjrRc--/t_card-square/v1654289492/qct07f275e8dc9ovy3r4.jpg',
+      'https://images.reverb.com/image/upload/s--L79HAaWW--/t_card-square/v1654289492/dkfnsy2jlf7vq90sytf8.jpg',
+    ],
+  },
+  {
+    id: '46931858',
+    title: 'Schecter Guitar Research P-4 4 String Ivory Black Pickguard',
+    condition: 'B-Stock',
+    strings: 4,
+    pricing: {
+      display: '$719.20',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--kgH_nuNL--/t_card-square/v1637235485/ptsij00nxpbj6goegubh.jpg',
+      'https://images.reverb.com/image/upload/s--rkvqWcMu--/t_card-square/v1637235487/mghe9on3burbn2ag48vr.jpg',
+      'https://images.reverb.com/image/upload/s--0u_JIFKn--/t_card-square/v1637235487/qjc2b9yjmor1yhsyxp8y.jpg',
+    ],
+  },
+  {
+    id: '56109814',
+    title: 'Ibanez SR1605B Premium 2021 Caribbean Shoreline Blue',
+    condition: 'Mint',
+    strings: 5,
+    pricing: {
+      display: '$1265.79',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--dptM8B2n--/t_card-square/v1654609773/bjxxwkeocb2wy1hrm4f7.jpg',
+      'https://images.reverb.com/image/upload/s--N2ah3yJS--/t_card-square/v1654609774/wagf02xmi8qc6orldfjk.jpg',
+      'https://images.reverb.com/image/upload/s--tKkVWce7--/t_card-square/v1654609775/fqadsyj12hkrbgnuh3sc.jpg',
+    ],
+  },
+  {
+    id: '56109587',
+    title: 'Squier Vintage Modified Jazz Bass Fretless',
+    condition: 'Very Good',
+    strings: 4,
+    pricing: {
+      display: '$375',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--ddM_IyrF--/t_card-square/v1654608791/vd1nzrcidn4kaezmgag4.jpg',
+      'https://images.reverb.com/image/upload/s--qnMbSvnO--/t_card-square/v1654608830/rnek7fbcybp4kaaimmtc.jpg',
+      'https://images.reverb.com/image/upload/s--uKIglWSF--/t_card-square/v1654608721/myhed1veug6ubmjib6q7.jpg',
+    ],
+  },
+  {
+    id: '56106074',
+    title: 'Ernie ball music man Stingray 4 lined fretless 1995',
+    condition: 'Excellent',
+    strings: 4,
+    pricing: {
+      display: '$2500',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--_3Zz4YWc--/t_card-square/v1654606293/nbdwpche13aygftezrfl.jpg',
+      'https://images.reverb.com/image/upload/s--RKI5TDQU--/t_card-square/v1654606291/s7adfa5ht5pm84aifgez.jpg',
+      'https://images.reverb.com/image/upload/s--bWWevVfd--/t_card-square/v1654606292/swhedvqighmnsxbyqwyu.jpg',
+    ],
+  },
+  {
+    id: '56089465',
+    title: 'Alembic Spoiler Cocobolo 7 String Bass',
+    condition: 'Excellent',
+    strings: 7,
+    pricing: {
+      display: '$5799.99',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--t_XfPJAU--/t_card-square/v1654571386/h3meeubsw2chb2olysld.jpg',
+      'https://images.reverb.com/image/upload/s--KvW5qymN--/t_card-square/v1654571394/ctmnxjdeyzvin3lbs4xo.jpg',
+      'https://images.reverb.com/image/upload/s--43WdHUqp--/t_card-square/v1654571402/x1es8iwk87aoiw6k0cer.jpg',
+    ],
+  },
+  {
+    id: '56105841',
+    title: 'Kiesel JB4 Jazz Bass with Case 2021 Vintage Cream',
+    condition: 'Excellent',
+    strings: 4,
+    pricing: {
+      display: '$1275',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--xIA-fQ-r--/t_card-square/v1654605013/kzra4bsq7mtoqhbcnshb.jpg',
+      'https://images.reverb.com/image/upload/s--RKI5TDQU--/t_card-square/v1654606291/s7adfa5ht5pm84aifgez.jpg',
+      'https://images.reverb.com/image/upload/s--bWWevVfd--/t_card-square/v1654606292/swhedvqighmnsxbyqwyu.jpg',
+    ],
+  },
+  {
+    id: '50325015',
+    title: 'Ibanez TMB100 Talman Bass',
+    condition: 'Mint',
+    strings: 4,
+    pricing: {
+      display: '$180',
+      currency: 'USD',
+    },
+    images: [
+      'https://images.reverb.com/image/upload/s--eT-JLyr1--/t_card-square/v1643947527/nhgygpawn2tvjv79bsqd.jpg',
+      'https://images.reverb.com/image/upload/s--hNf9LIkS--/t_card-square/v1643947528/yn2ltezohrg56a2rudhh.jpg',
+      'https://images.reverb.com/image/upload/s--Em4DCXPO--/t_card-square/v1643947527/a9zzysffwfxks7qc2zwn.jpg',
+    ],
+  },
+];
+
+export default basses;

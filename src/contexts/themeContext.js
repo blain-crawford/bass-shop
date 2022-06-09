a98c5958db8331fbbb3f37b66c10edc0b5c601cb
Blain Crawford <blaincrawford@gmail.com>
2022-06-09 09:23:31 -0400

    continuing to work on state management setup

--- src/contexts/themeContext.js
+++ src/contexts/themeContext.js
@@ -3,11 +3,16 @@ import React, { createContext, useState } from 'react';
 export const ThemeContext = createContext();
 
 const ThemeContextProvider = (props) => {
-  const [cartCount, setCartCount] = useState(1);
+  const [cartCount, setCartCount] = useState(0);
   const [currentCartContents, setCurrentCartContents] = useState([]);
   return (
     <ThemeContext.Provider
-      value={{cartCount}}
+      value={{
+        cartCount,
+        setCartCount,
+        currentCartContents,
+        setCurrentCartContents,
+      }}
     >
       {props.children}
     </ThemeContext.Provider>

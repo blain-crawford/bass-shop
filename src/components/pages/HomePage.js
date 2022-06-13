5f7bf091f457171fd3fb30b35d0a2fbc1693fe42
Blain Crawford <blaincrawford@gmail.com>
2022-06-13 16:07:09 -0400

    beginning to style home page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import allProducts from '../products/allProducts';
 import Header from '../Header';
-
+import styles from '../css/HomePageStyles.module.css'
 function HomePage() {
   return (
-    <div>
-      <h1>Hello, Main Page!</h1>
-    </div>
+    <main className={styles.mainPageContainer}>
+      <h1 className={styles.pageTitle}>Your One Stop Shop for all things bass gear</h1>
+      <div className={styles.previewContainer}>
+        <div className={styles.productPreview}></div>
+        <div className={styles.productPreview}></div>
+      </div>
+    </main>
   );
 }
 
